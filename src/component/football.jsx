31998e71995d2d74d06ca1d1b34855ebc87361dd
hopperhuang <huanjiwe@126.com>
2017-07-15 17:28:37 +0800

    增加了football组件做测试

---
+++ src/component/football.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button } from 'antd';
+
+class Football extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 1,
+    };
+    this.addNumber = this.addNumber.bind(this);
+  }
+  componentWillMount() {
+    console.log('i will mount');
+  }
+  addNumber() {
+    let { number } = this.state;
+    number += 1;
+    this.setState({ number });
+  }
+  render() {
+    return (
+      <div>
+        Football: {this.state.number}
+        <Button onClick={this.addNumber}>addNumber</Button>
+      </div>
+    );
+  }
+}
+export default Football;
